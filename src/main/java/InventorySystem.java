ac8f8ca75575e718f226ca087f2122e3facda8c5
K4m1katze <bszocker007@gmail.com>
2024-12-17 13:19:00 +0100

    added 3 filters vegan, prize and weight

--- src/main/java/InventorySystem.java
+++ src/main/java/InventorySystem.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class InventorySystem {
-    private final ArrayList<Bag> bags = new ArrayList<Bag>();
+    private final List<Bag> bags = new ArrayList<Bag>();
 
     public InventorySystem() {
         initObjekte();
@@ -13,19 +14,51 @@ public class InventorySystem {
         bags.add(new Bag("Blau", 0.5, false, 9.99));
     }
 
-    // takes a color and a list of bags as arguments
-    // returns only the bags in the list that had the color specified in the argument
-    public  ArrayList<Bag> filterColor(String color, ArrayList<Bag> bags) {
-        ArrayList<Bag> filteredBags = new ArrayList<Bag>();
+    public  List<Bag> filterColor(String color) {
+        List<Bag> filteredBags = new ArrayList<Bag>();
         for (Bag b : bags) {
             if (b.getColor().equals(color)) {
                 filteredBags.add(b);
             }
         }
         return filteredBags;
-    }
-    // TODO: 3 more filters for filterWeight, filterVegan and filterPrice.
 
+    }
+    public List<Bag> filterWeight(Double weight) {
+        List<Bag> filteredBags = new ArrayList<Bag>();
+        for (Bag a : bags) {
+            if (a.getWeight() == weight) {
+                filteredBags.add(a);
+            }
+        }
+        return filteredBags;
+    }
+    public List<Bag> filterisVegan(Boolean isVegan) {
+        List<Bag> filteredBags = new ArrayList<Bag>();
+        for (Bag c : bags) {
+            if (c.isVegan() == isVegan){
+                filteredBags.add(c);
+            }
+        }
+        return filteredBags;
+    }
+
+    public List<Bag> filterPrice(Double price ) {
+        List<Bag> filteredBags = new ArrayList<Bag>();
+        for (Bag a : bags) {
+            if (a.getPrice() == price) {
+                filteredBags.add(a);
+            }
+        }
+        return filteredBags;
+    }
+
+
+
+
+    // TODO: 1-2 new Attributes like brand and adding new Bag examples
+    // TODO: 3 more filters for weight, isVegan and price.
+    // TODO(optional): 2 more filter methods for the price (returning bags that are cheaper or more expensive than the specified price)
 
     // Setter and Getter methods
     public void addBag(String color, double weight, boolean isVegan, double price) {
@@ -36,7 +69,7 @@ public class InventorySystem {
         return bags.remove(bag);
     }
 
-    public ArrayList<Bag> getBags() {
+    public List<Bag> getBags() {
         return bags;
     }
 }
