0293de0959d9052f7dc2727a36bfffc95fe50b2f
Overthr0w <overthr0w138@gmail.com>
2025-01-13 18:16:14 +0100

    Cleaned up the code a bit

--- src/test/java/BagTest.java
+++ src/test/java/BagTest.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BagTest {
     final Bag testBag = new Bag("schwarz", 10.0, false, 14.99);
-    // TODO: add tests to the setters/getters (one assertion for each should be enough)
+
     @org.junit.jupiter.api.Test
     void getColor() {
         assertEquals("schwarz", testBag.getColor());
@@ -11,30 +11,30 @@ class BagTest {
     @org.junit.jupiter.api.Test
     void setColor() {
         testBag.setColor("magenta");
-        assertEquals(testBag.getColor(), "magenta");
+        assertEquals("magenta", testBag.getColor());
 
     }
 
     @org.junit.jupiter.api.Test
     void getWeight() {
-        assertEquals(10.0,testBag.getWeight());
+        assertEquals(10.0, testBag.getWeight());
     }
 
     @org.junit.jupiter.api.Test
     void setWeight() {
         testBag.setWeight(10.23);
-        assertEquals(testBag.getWeight(),10.23);
+        assertEquals(10.23, testBag.getWeight());
     }
 
     @org.junit.jupiter.api.Test
     void isVegan() {
-        assertEquals(false ,testBag.isVegan());
+        assertFalse(testBag.isVegan());
     }
 
     @org.junit.jupiter.api.Test
     void setVegan() {
         testBag.setVegan(true);
-        assertEquals(testBag.isVegan(), true);
+        assertTrue(testBag.isVegan());
     }
 
     @org.junit.jupiter.api.Test
@@ -45,6 +45,6 @@ class BagTest {
     @org.junit.jupiter.api.Test
     void setPrice() {
         testBag.setPrice(24.99);
-        assertEquals(testBag.getPrice(), 24.99);
+        assertEquals(24.99, testBag.getPrice());
     }
 }
